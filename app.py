2e534316cc8fc31aa307bfbec3e5b91ac8e61c11
Diksha <diksha252006@gmail.com>
2025-10-24 22:59:54 +0530

    updated

--- app.py
+++ app.py
@@ -22,6 +22,6 @@ st_lottie(home_animation, height=300)
 st.markdown("Welcome to the Fake News Detector App. Upload news files and check whether it's **Real** or **Fake**.")
 
 if st.button("🚀 Get Started"):
-    st.switch_page("Pages/predict.py")
+    st.switch_page("predict.py")
 
 # python -m streamlit run app.py
